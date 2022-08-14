c4a7bdceb068fce49da6b470fbdad344af5ea1de
tolya.sheremet.92@mail.ru <tolya.sheremet.92@mail.ru>
2022-08-14 16:34:30 +0300

    Перенес файлы в папку task1

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+int number = new Random().Next(10,100);
+Console.WriteLine($"Случайное число = {number}" );
+
+int digit1 = number / 10;
+int digit2 = number % 10;
+
+Console.WriteLine($"Первая цифра числа =  {digit1}" );
+Console.WriteLine($"Вторая цифра числа =  {digit2}" );
+
+if (digit1 > digit2)
+{
+   Console.WriteLine($"Цифра {digit1} наибольшее");
+}
+ else 
+ {
+    Console.WriteLine($"Цифра {digit2} наибольшее" );
+ }
